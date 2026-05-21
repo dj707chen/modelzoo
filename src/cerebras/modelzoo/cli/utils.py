6cd777f12ed2b5558947790332c8521f86b83283
DJ Chen <80285637+dj707chen@users.noreply.github.com>
2026-05-20 21:48:06 -0500

    Save running pretraining tutorial

--- src/cerebras/modelzoo/cli/utils.py
+++ src/cerebras/modelzoo/cli/utils.py
@@ -131,8 +131,9 @@ def _args_to_params(args, validate=True, extra_legacy_mapping_fn=None):
             params,
             extra_legacy_mapping_fn=extra_legacy_mapping_fn,
         )
-
-    print("_args_to_params:")
+    print("_args_to_params, args:")
+    pprint.pprint(vars(args))
+    print("_args_to_params, params:")
     pprint.pprint(params)
     return params
 
